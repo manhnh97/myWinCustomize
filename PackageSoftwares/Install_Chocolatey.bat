ed483b4a2cd39eb8c9fff2f0422a99facf95469e
Manhihi <manhnh97@gmail.com>
2023-11-05 00:40:08 +0700

    Update Install_Chocolatey.bat
    Updated!!!

--- PackageSoftwares/Install_Chocolatey.bat
+++ PackageSoftwares/Install_Chocolatey.bat
@@ -3,12 +3,11 @@
 :: Installing Powershell.exe chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-REM ========== START INSTALL PACKAGES ========== 
 ECHO ========== START INSTALL PACKAGES ========== 
 
 REM ========== SET VARIABLE for CHOCOLATEY ========== 
 set "PS=Powershell.exe choco install"
-set "Force=-vy"
+set "Force=-y"
 
 call :packagePERSONAL
 
@@ -21,14 +20,16 @@ exit /B 0
 		REM ========== CONTACTS ==========
 		rem %PS% skype %force%
 		rem %PS% zalopc %force%
-		rem %PS% telegram %force%
+		%PS% telegram %force%
+		%PS% discord %force%
+		%PS% zoom %force%
 
 		REM ========== TEXT EDITOR ==========
 		rem %PS% sublimetext3 %force%
-		rem %PS% notepadplusplus %force%
+		%PS% notepadplusplus %force%
 		rem %PS% "microsoft-windows-terminal" %force%
 		rem %PS% neovim %force%
-		@REM %PS% Vim %force%
+		rem %PS% Vim %force%
 		%PS% cascadiacode %force%
 
 		REM ========== IDE for CODE ==========
@@ -37,29 +38,25 @@ exit /B 0
 		:: IDE Java Programming Language
 		rem %PS% eclipse %force%
 		:: IDE C++/C#
-		%PS% visualstudio2019community %force%
+		%PS% visualstudio2022community %force%
 
-		%PS% python %force%
-		%PS% autohotkey %force%
-		%PS% autoit %force%
+		%PS% python3 %force%
+		rem %PS% autohotkey %force%
+		rem %PS% autoit %force%
 
 		REM ========== AUTOMATION TESTING ==========
-		:: %PS% postman %force%
+		%PS% postman %force%
 		:: %PS% jmeter %force%
-		@REM %PS% selenium %force%
+		%PS% selenium %force%
 		
 		REM ========== EXTENSIONS ==========
 		%PS% git %force%
-		%PS% dotnet-5.0-sdk %force%
-		%PS% pip %force%
-		%PS% scite4autoit3 %force%
-		@REM %PS% selenium %force%
-		@REM %PS% chromedriver %force%
-
+		rem %PS% scite4autoit3 %force%
+		%PS% chromedriver %force%
 
 		REM ========== WEB BROWSERS ==========
-		%PS% google-chrome-x64 %force%
-		%PS% tor-browser %force%
+		%PS% googlechrome %force%
+		rem %PS% tor-browser %force%
 		%PS% firefox %force%
 
 		REM ========== VPNs ==========
@@ -94,21 +91,22 @@ exit /B 0
 		:: Folders Management
 		%PS% onecommander %force%
 
-
 		REM ========== UTILITIES ==========
 		:: Capture Screen
+		%PS% qbittorrent %force%
 		%PS% greenshot %force%
 		%PS% screenpixelruler %force%
 		%PS% zoomit %force%
 		:: Record Press Keyboard
-		%PS% carnac %force%
+		rem %PS% carnac %force%
 		:: Pomodoro Timer
-		%PS% tomighty %force%
+		rem %PS% tomighty %force%
 
 		REM ========== OTHER TOOLS ==========
+		%PS% stremio %force%
 		%PS% vlc %force%
 		%PS% 7zip %force%
 		%PS% winrar %force%
-		%PS% spotify %force%
+		rem %PS% spotify %force%
 	endlocal
 	goto :eof
